ac12bd4c0f45945b060cce801d4aa90e0cad1862
AldoCap <capurro.ac@gmail.com>
2023-06-29 21:43:11 -0300

    Complica con el front end

---
+++ tickets/src/main.js
@@ -0,0 +1,24 @@
+import { calculatorView } from '../calculator/view.js';
+import { calculatorModel } from '../calculator/model.js'; 
+import { calculatorController } from '../calculator/controler.js'; 
+
+class calculator 
+{
+    constructor() 
+    {
+        this.innerView = new calculatorView(); 
+        this.innerModel = new calculatorModel();
+        this.innerController = new calculatorController(this.innerView, this.innerModel);
+        
+    }
+}; 
+
+function main() 
+{
+    let calculatorInstance = new calculator();
+
+    document.body.appendChild(calculatorInstance.innerView.container);
+};
+
+//window.addEventListener('load', main);
+window.onload = main;
